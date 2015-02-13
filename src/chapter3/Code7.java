d78b5a96a1258ad18f8e899d4f6033fda2326db8
wangshengzhong <632618392@qq.com>
2015-02-13 09:41:37 +0800

    新章节chapter3

---
+++ src/chapter3/Code7.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+import java.util.*;
+
+public class Code7 {
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input");
+		int k = in.nextInt();
+		System.out.println("Input is :" + k);
+		
+		
+		int[] num = new int[10];
+		for(int a : num)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
