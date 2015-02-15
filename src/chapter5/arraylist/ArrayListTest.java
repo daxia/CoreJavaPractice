b224dfb6095722ef2a9e0471fc0ce5244e83606c
wangshengzhong <632618392@qq.com>
2015-02-15 15:02:46 +0800

    new commit

---
+++ src/chapter5/arraylist/ArrayListTest.java
@@ -0,0 +1,15 @@
+package chapter5.arraylist;
+
+import java.util.ArrayList;
+
+import chapter5.equals.Employee;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Employee> staff = new ArrayList<Employee>();
+
+	}
+
+}
