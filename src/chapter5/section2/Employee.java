1147e9a69fc25e2f34ea1472ab4fa544b5916d3d
wangshengzhong <632618392@qq.com>
2015-02-15 11:54:09 +0800

    Commit Employee Class

---
+++ src/chapter5/section2/Employee.java
@@ -0,0 +1,76 @@
+package chapter5.section2;
+
+import java.util.*;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Employee {
+
+	private String name;
+	private double salary;
+	private Date hireDay;
+	
+	public Employee(String n, double s, int year, int month, int day)
+	{
+		name    = n;
+		salary  = s;
+		GregorianCalendar calendar = new GregorianCalendar(year, month , day);
+		hireDay = calendar.getTime();
+		
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getSalary()
+	{
+		return salary;
+	}
+	
+	public Date getHireDay()
+	{
+		return hireDay;
+	}
+	
+	public void raiseSalary(double byPercent)
+	{
+		double raise = salary * byPercent /100;
+		salary += raise;
+	}
+	
+	public boolean equals(Object otherObject)
+	{
+		if (this == otherObject) return true;
+		
+		if (otherObject == null) return false;
+		
+		if (this.getClass() != otherObject.getClass()) return false;
+		
+		Employee other = (Employee) otherObject;
+		
+		return Objects.equals(this.name, other.name) && 
+				this.salary == other.salary &&  
+				Objects.equals(this.hireDay, other.hireDay);
+		
+	}
+	
+	public int hashCode()
+	{
+		return Objects.hash(name, salary, hireDay);
+		
+	}
+	
+	public String toString()
+	{
+		return this.getClass().getName() + 
+				"[name=" + name + 
+				",salary=" + salary +
+				",hireDay=" + hireDay +
+				"]";
+				
+	}
+	
+	
+}
