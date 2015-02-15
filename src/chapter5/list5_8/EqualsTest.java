cb73fedc45fdc5174015fb5091ef96dec6c3ce61
wangshengzhong <632618392@qq.com>
2015-02-15 11:32:18 +0800

    包目录修改

---
+++ src/chapter5/list5_8/EqualsTest.java
@@ -0,0 +1,19 @@
+package chapter5.list5_8;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class EqualsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Employee{
+	
+}
