dbadfcd37887cd84d117675a06a03b2f88f1c0c4
wangshengzhong <632618392@qq.com>
2015-02-15 11:23:02 +0800

    chapter4 改为chapter5

---
+++ src/chapter5/Code_1_HashTable.java
@@ -0,0 +1,16 @@
+package chapter5;
+
+public class Code_1_HashTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "wang";
+		System.out.println(s.hashCode());
+		String t = "wang";
+		System.out.println(t.hashCode());
+
+		System.out.println(s.equals(t));
+		
+	}
+
+}
---
+++ src/chapter5/Listing5_8_EqualsTest.java
@@ -0,0 +1,10 @@
+package chapter5;
+
+public class Listing5_8_EqualsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
