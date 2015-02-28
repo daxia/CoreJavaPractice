e75cb648e539e96f40a536beef102a0f3d84e038
wangshengzhong <632618392@qq.com>
2015-02-28 12:42:58 +0800

    add

--- src/chapter5/arraylist/ArrayListTest.java
+++ src/chapter5/arraylist/ArrayListTest.java
@@ -9,7 +9,21 @@ public class ArrayListTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Employee> staff = new ArrayList<Employee>();
-
+		staff.add(new Employee("wang", 56000, 2012, 1, 1));
+		staff.add(new Employee("sheng", 56000, 2012, 1, 1));
+		staff.add(new Employee("zhong", 56000, 2012, 1, 1));
+		
+		for(Employee employee: staff)
+		{
+			employee.raiseSalary(10);
+		}
+		
+		for(Employee employee : staff)
+		{
+			System.out.println("name=" + employee.getName() + 
+					",salary=" + employee.getSalary() + 
+					",hireDay=" + employee.getHireDay());
+		}
 	}
 
 }
