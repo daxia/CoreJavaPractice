8217303b1c0cf4246f07b05dc4e5f6b229d74a8d
wangshengzhong <632618392@qq.com>
2015-03-12 09:43:26 +0800

    add chapter 7.4 DrawTest

--- src/chapter7/_4_draw/DrawTest.java
+++ src/chapter7/_4_draw/DrawTest.java
@@ -1,15 +1,18 @@
 package chapter7._4_draw;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.color.ColorSpace;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
+import java.lang.ProcessBuilder.Redirect;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -55,16 +58,20 @@ class DrawComponent extends JComponent{
 		double width   = 200;
 		double height = 150;
 		
+		//draw rectangle
 		Rectangle2D rect = new Rectangle2D.Double(LeftX, topY, width, height);
 		g2d.draw(rect);
 		
+		//draw ellipse
 		Ellipse2D ellipse = new Ellipse2D.Double();
 		ellipse.setFrame(rect);
 		g2d.draw(ellipse);
 		
+		//draw line
 		g2d.draw(new Line2D.Double(LeftX, topY, width, height));
 		
 		
+		//draw circle
 		double CenterX = rect.getCenterX();
 		double CenterY = rect.getCenterY();
 		double radius = 150;
@@ -75,8 +82,9 @@ class DrawComponent extends JComponent{
 		g2d.draw(circle);
 		
 		
-		
-		
+		g2d.setColor(Color.red);
+
+		g2d.drawString("wang", (int)LeftX, (int)topY);
 		
 		
 		//super.paintComponent(g);
